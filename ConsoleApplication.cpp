4a514e4d07b84dbf23cbd397e5663504cdb0a8a4
cetrin2 <38029626+cetrin2@users.noreply.github.com>
2023-01-12 21:13:19 +0300

    Add files via upload

---
+++ ConsoleApplication.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication.cpp: определяет точку входа для консольного приложения.
+//
+
+
+#include "stdafx.h"
+#include <windows.h>
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello\n");
+	system("pause");
+}
